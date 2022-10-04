9282f72230a70a241352535e68e4a14c0cc33c9e
robinetta <alxr19@gmail.com>
2022-10-04 15:38:49 -0400

    first two exercises complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    
+    if (Math.sign(num) === -1){
+        return "ERROR"
+    }else {
+        let newStr = ""
+        for(let i = 0; i < num; i++){
+            newStr += string
+            
+        }
+        return newStr 
+    }
+  
+    
 };
 
 // Do not edit below this line
